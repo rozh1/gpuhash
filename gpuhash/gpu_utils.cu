adb7f515e0f278cb11c91e4464c96fcc125fa707
Roman Klassen <roman.klassen@comp-gluk.ru>
2018-01-21 13:43:51 +0300

    Добавлена лицензия

--- gpuhash/gpu_utils.cu
+++ gpuhash/gpu_utils.cu
@@ -1,10 +1,23 @@
+/*
+* Copyright 2017-2018 Roman Klassen
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*
+*/
+
 #include "gpu_utils.cuh"
 #include "cuda_runtime.h"
 
-
-//=============================================================================
-//Memory Management
-//=============================================================================
 void* gc_malloc(size_t bufsize)
 {
 	void* gpu_buf = NULL;
@@ -34,11 +47,7 @@ void* gc_device2host(gcStream_t Stream, void* gpu_buf, size_t bufsize)
 	void* pinned = NULL;
 	CUDA_SAFE_CALL(cudaMallocHost((void**)&pinned, bufsize));
 	CUDA_SAFE_CALL(cudaMemcpyAsync(pinned, gpu_buf, bufsize, cudaMemcpyDeviceToHost, (CUstream_st*)Stream.stream));
-	//void* cpu_buf = malloc(bufsize);
-	//memcpy(cpu_buf, pinned, bufsize);
-	//CUDA_SAFE_CALL(cudaFreeHost(pinned));
 
-	//return cpu_buf;
 	return pinned;
 }
 
